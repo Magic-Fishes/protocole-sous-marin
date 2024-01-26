3864d4d324b85b021e0794cb3668f1626b646190
saumon-brule <75emilien75@gmail.com>
2024-01-26 01:13:01 +0100

    feat: display message to tell if the bot is in dev or prod mod

--- src/events/ready.js
+++ src/events/ready.js
@@ -5,6 +5,7 @@ module.exports = {
     once: true,
     async execute(Client) {
         console.log("Bot ON");
+        Client.channels.cache.get(config.channels.feedback).send((config.dev ? "BOT lancé en DEVELOPPEMENT" : "BOT lancé en PRODUCTION"));
         // Client.guilds.cache.get(config.server).commands.set(Client.commands.map(cmd => cmd)) // Used to activade commands but we don't have any with this bot
     }
 }
